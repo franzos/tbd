a5dc9754608ea4dee97cb25e067b687775763371
Franz Geffke <franz@pantherx.org>
2023-07-16 00:28:42 +0100

    expand entries query params #2

--- model/entry_query.go
+++ model/entry_query.go
@@ -1,12 +1,14 @@
 package model
 
 type EntryQueryParams struct {
-	Offset    int    `query:"offset" validate:"omitempty,number,min=0"`
-	Limit     int    `query:"limit" validate:"omitempty,number,min=1"`
-	Type      string `query:"type"`
-	Price     string `query:"price"`
-	StartDate string `query:"start_date"`
-	EndDate   string `query:"end_date"`
-	Country   string `query:"country"`
-	City      string `query:"city"`
+	Offset     int    `query:"offset" validate:"omitempty,number,min=0"`
+	Limit      int    `query:"limit" validate:"omitempty,number,min=1"`
+	Type       string `query:"type"`
+	Price      string `query:"price"`
+	StartDate  string `query:"start_date"`
+	EndDate    string `query:"end_date"`
+	Country    string `query:"country"`
+	City       string `query:"city"`
+	CitySlug   string `query:"city_slug"`
+	CityGlobID string `query:"city_glob_id"`
 }
