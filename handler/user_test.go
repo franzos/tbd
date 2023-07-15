0c39be82c2da3b6fb95cc52c4a435ffe1d043c3b
Franz Geffke <franz@pantherx.org>
2023-07-16 00:29:22 +0100

    expanded user tests to upload image

--- handler/user_test.go
+++ handler/user_test.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -52,6 +54,37 @@ func signupAndLogin(t *testing.T) string {
 	assert.NoError(t, err)
 
 	return loginResponse.Token
+}
+
+func updateUser(t *testing.T, token string, userData map[string]interface{}) {
+	userURL := "http://localhost:1323/account/me"
+
+	rec := performRequest(t, http.MethodPatch, userURL, token, userData)
+
+	if rec.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status OK, got: %v", rec.StatusCode)
+	}
+}
+
+func updateUserImage(t *testing.T, token string, image model.File) {
+	updateURL := "http://localhost:1323/account/me"
+	updateData := map[string]interface{}{
+		"image": image,
+	}
+
+	payload, _ := json.Marshal(updateData)
+
+	req, err := http.NewRequest(http.MethodPatch, updateURL, bytes.NewBuffer(payload))
+	assert.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := http.Client{}
+	res, err := client.Do(req)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
 }
 
 func TestSignup(t *testing.T) {
@@ -175,6 +208,91 @@ func TestAccountMe(t *testing.T) {
 
 	// Assertions for requesting user profile
 	assert.Equal(t, http.StatusOK, accountRec.StatusCode)
+}
+
+func TestAccountMeUpdate(t *testing.T) {
+	token := signupAndLogin(t)
+
+	// Create a patch to update the profile
+	updateData := map[string]interface{}{
+		"profile": map[string]interface{}{
+			"links": []map[string]interface{}{
+				{
+					"url":  "https://twitter.com/user",
+					"name": "Twitter",
+				},
+			},
+		},
+	}
+
+	// Update the user profile
+	updateUser(t, token, updateData)
+
+	// Use the token to request the user's own profile
+	accountURL := "http://localhost:1323/account/me"
+	accountReq, _ := http.NewRequest(http.MethodGet, accountURL, nil)
+	accountReq.Header.Set("Authorization", "Bearer "+token)
+
+	client := http.Client{}
+	accountRec, err := client.Do(accountReq)
+	assert.NoError(t, err)
+
+	// Assertions for requesting user profile
+	assert.Equal(t, http.StatusOK, accountRec.StatusCode)
+
+	// Read the response body
+	body, _ := ioutil.ReadAll(accountRec.Body)
+	defer accountRec.Body.Close()
+
+	// Parse the response into a map
+	data := make(map[string]interface{})
+	json.Unmarshal(body, &data)
+
+	fmt.Println(data)
+
+	// Extract the profile from the response
+	profile := data["profile"].(map[string]interface{})
+
+	// Check the profile has been updated
+	expectedLinks := []interface{}{
+		map[string]interface{}{
+			"url":  "https://twitter.com/user",
+			"name": "Twitter",
+		},
+	}
+	assert.Equal(t, expectedLinks, profile["links"])
+}
+
+func TestAccountMeUpdateImage(t *testing.T) {
+	token := signupAndLogin(t)
+
+	files := uploadFiles(t, token, "../concorde.jpg")
+
+	// Update user image
+	updateUserImage(t, token, files[0])
+
+	// Get the updated user profile
+	accountURL := "http://localhost:1323/account/me"
+	accountReq, _ := http.NewRequest(http.MethodGet, accountURL, nil)
+	accountReq.Header.Set("Authorization", "Bearer "+token)
+
+	client := http.Client{}
+	accountRec, err := client.Do(accountReq)
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, accountRec.StatusCode)
+
+	var accountResponse struct {
+		User struct {
+			Image model.File `json:"image"`
+		} `json:"user"`
+	}
+
+	err = json.NewDecoder(accountRec.Body).Decode(&accountResponse)
+	assert.NoError(t, err)
+
+	// Check if the image is correctly updated
+	assert.Equal(t, files[0].ID, accountResponse.User.Image.ID)
 }
 
 func TestUserDeleteSelf(t *testing.T) {
